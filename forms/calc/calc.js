19c0e6c8c4f9cedf0afd3e1d41dd43c52c6b2401
HaleyGilbertson <hag79621@creighton.edu>
2021-09-22 15:09:16 -0500

    string

---
+++ forms/calc/calc.js
@@ -0,0 +1,12 @@
+/*
+function Calculate(num1, num2) {
+   let answer = num1 + num2
+   return functionSum
+}
+
+let num1 = Number(prompt('Enter your first number:'))
+let num2 = Number(prompt('Enter your second number:'))
+
+let summedNumbers = calculate(num1, num2)
+console.log(`The sum of ${num1} and ${num2} is ${summedNumbers}`)
+*/
